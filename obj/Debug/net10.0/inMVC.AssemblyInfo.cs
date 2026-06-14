a52a28e3acc01f84cb89c42715b6b256f9853404
Zenkiee <carlos.ernaciooo@gmail.com>
2026-06-14 13:18:24 +0800

    css

--- obj/Debug/net10.0/inMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/inMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10479a4065b1d165eeb6ba718cf944b837d5ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd88ae0b457f454f7da89f66462bb4aec641171")]
 [assembly: System.Reflection.AssemblyProductAttribute("inMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
